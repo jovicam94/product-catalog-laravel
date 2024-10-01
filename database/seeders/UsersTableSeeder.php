902d98ee5d7f2d63be73bdb2170d0679efc1c4ed
Jovica Misirlic <jovica.misirlic@gmail.com>
2024-10-01 13:10:18 +0200

    Add seeders for inserting data

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->john_doe_state()->create();
+
+        User::factory(30)->create();
+    }
+}
