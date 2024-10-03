1cae0da2922ce362b14ec300044cda7cd709f1e1
Jovica Misirlic <jovica.misirlic@gmail.com>
2024-10-03 11:56:18 +0200

    Add relation for comment status

--- app/Models/CommentStatus.php
+++ app/Models/CommentStatus.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class CommentStatus extends Model
 {
     use HasFactory;
+
+    const STATUS_WAITING_FOR_APPROVAL = 1;
+    const STATUS_APPROVED = 2;
+    const STATUS_DENIED = 3;
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
